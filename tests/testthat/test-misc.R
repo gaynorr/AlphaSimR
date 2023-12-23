193a136cd9d4e55fb64a4be5d1ae0fc429e7d063
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2023-12-23 06:22:18 +0100

    Exploring subsetting and combining with Pop & MultiPop

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -2,29 +2,46 @@ test_that("misc_and_miscPop",{
   founderPop = quickHaplo(nInd=2, nChr=1, segSites=10)
   SP = SimParam$new(founderPop)
   SP$addTraitA(10)
-  pop = newPop(founderPop, simParam=SP)
+  popOrig = newPop(founderPop, simParam=SP)
+  multiPop = newMultiPop(popOrig, popOrig)
 
-  popSub = pop[1]
+  popSub = popOrig[1]
   expect_equal(popSub@misc, list())
   expect_equal(popSub@miscPop, list())
 
-  pop@misc$tmp1 = rnorm(n=2)
-  pop@misc$tmp2 = rnorm(n=2)
-  pop@miscPop$tmp1 = sum(pop@misc$tmp1)
-  pop@miscPop$tmp2 = sum(pop@misc$tmp2)
+  pop = popOrig
+  pop@misc$vec = rnorm(n=2)
+  pop@misc$mtP = popOrig # hmm, should this actually be multiple pop objects or one with multiple individuals?
+  pop@misc$mtMP = multiPop
+  pop@miscPop$vec = sum(pop@misc$vec)
+  pop@miscPop$af = colMeans(pullSegSiteGeno(pop))
 
   popSub = pop[1]
-  expect_equal(popSub@misc$tmp1, pop@misc$tmp1[1])
-  expect_equal(popSub@misc$tmp2, pop@misc$tmp2[1])
+  expect_equal(popSub@misc$vec, pop@misc$vec[1])
+  expect_equal(popSub@misc$mtP, pop@misc$mtP[1])
+  expect_equal(popSub@misc$mtMP, pop@misc$mtMP[1])
   expect_equal(popSub@miscPop, list())
 
   popSub = pop[0]
-  expect_equal(popSub@misc$tmp1, numeric(0))
-  expect_equal(popSub@misc$tmp2, numeric(0))
+  expect_equal(popSub@misc$vec, numeric(0))
+  expect_equal(popSub@misc$mtP, newEmptyPop())
+  expect_equal(popSub@misc$mtMP, ???) # what should we test here against?
   expect_equal(popSub@miscPop, list())
 
-  popC = c(pop, pop)
-  expect_equal(popC@misc$tmp1, c(pop@misc$tmp1, pop@misc$tmp1))
-  expect_equal(popC@misc$tmp2, c(pop@misc$tmp2, pop@misc$tmp2))
+  popC = c(pop, pop) # this now breaks with validity check
+  # Error in validObject(.Object) :
+  #   invalid class “Pop” object: any(nInd!=sapply(misc, length))
+  # --> because length(pop@misc$vec) is 2, but length(pop@misc$mtP) is 1
+
+  pop@misc$mtP = NULL # removing mtP for now to test how it works with mtMP
+  popC = c(pop, pop) # this now breaks with validity check
+  # Error in validObject(.Object) :
+  #   invalid class “Pop” object: any(nInd!=sapply(misc, length))
+  # --> because length(pop@misc$vec) is 2, but length(pop@misc$mtMP) is 1
+  # the later should really be 2!
+
+  expect_equal(popC@misc$vec, c(pop@misc$vec, pop@misc$vec))
+  expect_equal(popC@misc$mtP, c(pop@misc$mtP, pop@misc$mtP))
+  expect_equal(popC@misc$mtMP, c(pop@misc$mtMP, pop@misc$mtMP))
   expect_equal(popC@miscPop, list())
 })
