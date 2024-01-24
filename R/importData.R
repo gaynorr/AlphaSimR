b62e5745e739a5acbd442be745b11388bd375a77
Chris Gaynor <gaynor.robert@hotmail.com>
2024-01-24 15:57:07 -0600

    fix for #175

--- R/importData.R
+++ R/importData.R
@@ -276,7 +276,7 @@ importHaplo = function(haplo, genMap, ploidy=2L, ped=NULL){
       genMap[[i]] = genMap[[i]] - genMap[[i]]-genMap[[i]][1]
       take = na.omit(take)
     }
-    haplotypes[[i]] = haplo[,take]
+    haplotypes[[i]] = haplo[,take,drop=FALSE]
   }
   
   founderPop = newMapPop(genMap=genMap,
