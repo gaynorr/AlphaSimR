878dc74874414cc57c6b680d094785b8124b78a0
Daniel Ariza Suarez <daniel.arizasuarez@usys.ethz.ch>
2025-10-20 15:25:11 +0100

    setPhenoPop: Remove support for HybridPop. Use isPop, isHybridPop and isMultiPop

--- tests/testthat/test-phenotypes.R
+++ tests/testthat/test-phenotypes.R
@@ -228,8 +228,6 @@ test_that("setPhenoPop", {
   )
 
   # 9) Invalid input type should error
-  expect_error(
-    setPhenoPop(123, simParam = SP),
-    "x must be an object of Pop, HybridPop or MultiPop class."
-  )
+  expect_error(setPhenoPop(123, simParam = SP), 
+               "x must be an object of Pop or MultiPop class.")
 })
