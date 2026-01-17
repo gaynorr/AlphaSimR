4e21e4ed746530da69e70215307e524c21f91576
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2026-01-16 17:41:40 -0800

    Sorting MaCS seed issue - take 2

---
+++ tests/testthat/test-reproducibility.R
@@ -0,0 +1,114 @@
+context("reproducibility")
+
+test_that("quickHaplo() and runMacs() are reproducible", {
+  # ---- quickHaplo() test ----
+
+  set.seed(123)
+  pop <- quickHaplo(nInd = 4, nChr = 2, segSites = 10)
+  hap1 <- pullSegSiteHaplo(pop)
+  hap1
+
+  set.seed(123)
+  pop <- quickHaplo(nInd = 4, nChr = 2, segSites = 10)
+  hap2 <- pullSegSiteHaplo(pop)
+  hap2
+
+  set.seed(124)
+  pop <- quickHaplo(nInd = 4, nChr = 2, segSites = 10)
+  hap3 <- pullSegSiteHaplo(pop)
+  hap3
+
+  test <- sum(hap1 - hap2) == 0
+  # print(test)
+  expect_true(test)
+
+  test <- !sum(hap1 - hap3) == 0
+  # print(test)
+  expect_true(test)
+
+  # ---- runMacs(..., nThreads = 1) test ----
+
+  set.seed(123)
+  pop <- runMacs(
+    nInd = 4,
+    nChr = 2,
+    segSites = 10,
+    species = "GENERIC",
+    nThreads = 1
+  )
+  hap1 <- pullSegSiteHaplo(pop)
+  hap1
+
+  set.seed(123)
+  pop <- runMacs(
+    nInd = 4,
+    nChr = 2,
+    segSites = 10,
+    species = "GENERIC",
+    nThreads = 1
+  )
+  hap2 <- pullSegSiteHaplo(pop)
+  hap2
+
+  set.seed(124)
+  pop <- runMacs(
+    nInd = 4,
+    nChr = 2,
+    segSites = 10,
+    species = "GENERIC",
+    nThreads = 1
+  )
+  hap3 <- pullSegSiteHaplo(pop)
+  hap3
+
+  test <- sum(hap1 - hap2) == 0
+  # print(test)
+  expect_true(test)
+
+  test <- !sum(hap1 - hap3) == 0
+  # print(test)
+  expect_true(test)
+
+  # ---- runMacs(..., nThreads = 2) test ----
+
+  set.seed(123)
+  pop <- runMacs(
+    nInd = 4,
+    nChr = 2,
+    segSites = 10,
+    species = "GENERIC",
+    nThreads = 2
+  )
+  hap1 <- pullSegSiteHaplo(pop)
+  hap1
+
+  set.seed(123)
+  pop <- runMacs(
+    nInd = 4,
+    nChr = 2,
+    segSites = 10,
+    species = "GENERIC",
+    nThreads = 2
+  )
+  hap2 <- pullSegSiteHaplo(pop)
+  hap2
+
+  set.seed(124)
+  pop <- runMacs(
+    nInd = 4,
+    nChr = 2,
+    segSites = 10,
+    species = "GENERIC",
+    nThreads = 2
+  )
+  hap3 <- pullSegSiteHaplo(pop)
+  hap3
+
+  test <- sum(hap1 - hap2) == 0
+  # print(test)
+  expect_true(test)
+
+  test <- !sum(hap1 - hap3) == 0
+  # print(test)
+  expect_true(test)
+})
